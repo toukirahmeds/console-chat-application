96ba20f8c3594b8f404e0c0401d67d9d5bad5345
Toukir Ahmed Shuva <toukirahmeds@gmail.com>
2024-02-25 22:52:09 +0600

    Implement prompt for protocol selection

--- promptProtocol.js
+++ promptProtocol.js
@@ -1,1 +1,27 @@
-console.log("Prompt for protocol");
+const cliSelect = require("cli-select");
+
+/**
+ * Loads protocol select list and returns the selected option.
+ * If cancelled, then `undefined` is returned.
+ * 
+ * @returns {string | undefined}
+ */
+const loadProtocolSelection = async () => {
+  console.log("What protocol do you want to use?");
+
+  try {
+    const selectedOption = await cliSelect({
+      values: [ "tcp", "udp", "http" ],
+      selected: "[\u2714]",
+      unselected: "[ ]"
+    });
+
+    const { value } = selectedOption;
+
+    return value;
+  } catch (error) {
+    return undefined;
+  }
+};
+
+module.exports = loadProtocolSelection;
